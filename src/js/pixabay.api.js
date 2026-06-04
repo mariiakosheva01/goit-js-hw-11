9ef61ffd37304d44b653dec19bd4a2b97e96f3f8
Maria <maria.koshevaya2808@gmail.com>
2026-06-04 18:57:58 +0200

    2

---
+++ src/js/pixabay.api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = 'Y56144354-a08717fc8df2b40204429c095'; 
+const BASE_URL = 'https://pixabay.com/api/';
+
+export function getImagesByQuery(query) {
+  const searchParams = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  };
+
+  return axios.get(BASE_URL, { params: searchParams })
+    .then(response => response.data);
+}
