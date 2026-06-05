f6b38eb8f27b736ab9dff018dab162a99d2455f0
Maria <maria.koshevaya2808@gmail.com>
2026-06-05 18:13:00 +0200

    $

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
 
-import { getImagesByQuery } from './js/pixabay.api.js';
+import { getImagesByQuery } from './js/pixabay-api.js';
 import { createGallery, clearGallery, showLoader, hideLoader } from './js/render-functions.js';
 
 const searchForm = document.querySelector('.form');
